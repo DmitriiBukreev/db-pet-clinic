4f2350d1d9a3bc67ea0882985eecc99be7995b06
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-03-11 11:59:54 +0100

    Initial commit

---
+++ src/main/java/db/springframework/dbpetclinic/DbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package db.springframework.dbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbPetClinicApplication.class, args);
+    }
+
+}
